0704e0474a17089a29514450dcebbfccc585cdbc
David Janes <davidjanes@davidjanes.com>
2016-01-23 09:28:21 -0500

    ...

---
+++ samples/connect_light.js
@@ -0,0 +1,30 @@
+/*
+ *  SEE "iotdb.js" for the best way to use this
+ *  Connect to a Hue and cycle through colors
+ */
+
+"use strict";
+
+var Plugfest = require('../PlugfestBridge').Bridge;
+
+var bridge_exemplar = new Plugfest({
+    url: 'coap://129.132.130.252:5082',
+});
+bridge_exemplar.discovered = function (bridge) {
+    console.log("+ got one\n ", bridge.meta());
+    bridge.pulled = function (state) {
+        console.log("+ state-change\n ", state);
+    };
+    bridge.connect({});
+
+    /*
+    var count = 0;
+    var colors = ["#FF0000", "#00FF00", "#0000FF", "#00FFFF", "#FF00FF", "#FFFF00", "#FFFFFF", "#000000"];
+    setInterval(function () {
+        bridge.push({
+            color: colors[count++ % colors.length]
+        }, function () {});
+    }, 2500);
+    */
+};
+bridge_exemplar.discover();
